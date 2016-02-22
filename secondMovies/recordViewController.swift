52c9e8d688665310685fb92245acf4e2164942de
Koichi <cx.w0077@gmail.com>
2016-02-23 00:46:35 +0800

    タイマー機能追加

--- secondMovies/recordViewController.swift
+++ secondMovies/recordViewController.swift
@@ -14,14 +14,20 @@ class recordViewController: UIViewController {
     var startButton, stopButton, pauseResumeButton, mergeButton: UIButton!
     var isRecording = false
     let cameraEngine = CameraEngine()
-    @IBOutlet weak var timerLabel: UILabel!
-    //時間計測用の変数.
-    var cnt : Float = 0
     
+    //@IBOutlet weak var timerLabel: UILabel!
+    
+    
+    //カメラボタンの作成
     var cameraBtn, finishBtn: UIButton!
     var cameraStatus = "readyToStart"
     
-    
+    //タイマーラベルの作成
+    var timerLabel: UILabel!
+    var timer : NSTimer!
+    //時間計測用の変数.
+    var cnt : Float = 0.00
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,16 +44,21 @@ class recordViewController: UIViewController {
         self.view.layer.addSublayer(videoLayer)
         //self.view.sendSubviewToBack(videoLayer)
         
+        //ボタンの配置
         self.setupButton()
         
-        //タイマーを作る.
-        //var timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        //タイマー配置
+        self.setupTimerLabel()
+        
     }
     
-    func update() {
-        //timerLabel.text = "撮影時間→"+String(cnt)
-        //cnt++
+    override func viewWillAppear(animated: Bool) {
+        var myDefault = NSUserDefaults.standardUserDefaults()
+        cnt = myDefault.floatForKey("defaultCnt")
+        timerLabel.text = String(cnt)
     }
+    
+    
     
 //    @IBAction func cameraBtnTap(sender: UIButton) {
 //        
@@ -103,6 +114,8 @@ class recordViewController: UIViewController {
 //        self.view.addSubview(self.stopButton)
 //        self.view.addSubview(self.pauseResumeButton)
 //        self.view.addSubview(self.mergeButton)
+        
+        //Viewにボタンを追加
         self.view.addSubview(self.cameraBtn)
         self.view.addSubview(self.finishBtn)
        
@@ -115,23 +128,38 @@ class recordViewController: UIViewController {
             if !self.cameraEngine.isCapturing {
                 self.cameraEngine.start()
                 self.cameraBtn.setTitle("Stop", forState: .Normal)
-                self.changeButtonColor(self.cameraBtn, color: UIColor.redColor())
+                self.changeButtonColor(self
+                    .cameraBtn, color: UIColor.redColor())
                 cameraStatus = "recording"
+                //撮影スタート時にカウント開始
+                var myDefault = NSUserDefaults.standardUserDefaults()
+                cnt = myDefault.floatForKey("defaultCnt")
+                timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+                print("1")
             }
         }else if cameraStatus == "recording"{
-            print("if一個目")
             if self.cameraEngine.isCapturing {
-                print("if2こめ")
                 if self.cameraEngine.isPaused {
-                    print("if3こめ")
+                    //一時停止後録画再開中がここの処理
                     self.cameraEngine.resume()
-                    self.cameraBtn.setTitle("一時停止中", forState: .Normal)
-                    self.cameraBtn.backgroundColor = UIColor.greenColor()
+                    self.cameraBtn.setTitle("停止中", forState: .Normal)
+                    self.cameraBtn.backgroundColor = UIColor.redColor()
+                    //ユーザーデフォルトのカウント数呼び出し
+                    var myDefault = NSUserDefaults.standardUserDefaults()
+                    cnt = myDefault.floatForKey("defaultCnt")
+                    timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+                    print("2")
                 }else{
-                    print("if3こめのelse")
+                    //一時停止した時にここの処理
                     self.cameraEngine.pause()
-                    self.cameraBtn.setTitle("resume", forState: .Normal)
+                    self.cameraBtn.setTitle("restart", forState: .Normal)
                     self.cameraBtn.backgroundColor = UIColor.greenColor()
+                    //ユーザーデフォルトにカウント数書き込み
+                    var myDefault = NSUserDefaults.standardUserDefaults()
+                    myDefault.setFloat(cnt, forKey: "defaultCnt")
+                    myDefault.synchronize()
+                    timer.invalidate()
+                    print("3")
                 }
             }
         }
@@ -141,11 +169,38 @@ class recordViewController: UIViewController {
     func finishBtnTap(sender:UIButton){
         if self.cameraEngine.isCapturing {
             self.cameraEngine.stop()
+            //ユーザーデフォルトにカウント数書き込み
+            cnt = 0.00
+            var myDefault = NSUserDefaults.standardUserDefaults()
+            myDefault.setFloat(cnt, forKey: "defaultCnt")
+            myDefault.synchronize()
+            timer.invalidate()
         }
         
     }
     
+    func setupTimerLabel(){
+        //timer定義
+        timerLabel.frame = CGRectMake(0,0,700,200)
+        timerLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 300)
+        timerLabel.backgroundColor = UIColor.redColor()
+        timerLabel.text = String(cnt)
+        timerLabel.font = UIFont.systemFontOfSize(12)
+        timerLabel.textColor = UIColor.whiteColor()
+        timerLabel.shadowColor = UIColor.blueColor()
+        timerLabel.textAlignment = NSTextAlignment.Center
+        timerLabel.layer.masksToBounds = true
+        timerLabel.layer.cornerRadius = 15.0
+
+        // Viewにtimerラベルを追加
+        self.view.addSubview(timerLabel)
+    }
     
+    //timerカウント関数
+    func update() {
+        timerLabel.text = "撮影時間→"+String(cnt)
+        cnt++
+    }
     
     
     
