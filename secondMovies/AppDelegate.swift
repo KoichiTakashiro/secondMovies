679deb209725c1589ba0b13a7ad332cc87839a64
Koichi <cx.w0077@gmail.com>
2016-03-01 20:55:39 +0800

    スプラッシュアニメーション追加

--- secondMovies/AppDelegate.swift
+++ secondMovies/AppDelegate.swift
@@ -16,8 +16,87 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // ▼ 1. windowの背景色にLaunchScreen.xibのviewの背景色と同じ色を設定
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.backgroundColor = UIColor(red: 247, green: 107, blue: 102, alpha: 1)
+        self.window!.makeKeyAndVisible()
+        
+        // rootViewController from StoryBoard
+        // ▼ 2. rootViewControllerをStoryBoardから設定 (今回はUINavigationControllerとしているが、他のViewControllerでも可)
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        var navigationController = mainStoryboard.instantiateViewControllerWithIdentifier("navigationController") as! UIViewController
+        self.window!.rootViewController = navigationController
+        
+        // logo mask
+        // ▼ 3. rootViewController.viewをロゴ画像の形にマスクし、LaunchScreen.xibのロゴ画像と同サイズ・同位置に配置
+        navigationController.view.layer.mask = CALayer()
+        navigationController.view.layer.mask!.contents = UIImage(named: "flamingo")!.CGImage
+        navigationController.view.layer.mask!.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        navigationController.view.layer.mask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        navigationController.view.layer.mask!.position = CGPoint(x: navigationController.view.frame.width / 2, y: navigationController.view.frame.height / 2)
+        
+        // logo mask background view
+        // ▼ 4. rootViewController.viewの最前面に白いviewを配置
+
+        var maskBgView = UIView(frame: navigationController.view.frame)
+        maskBgView.backgroundColor = UIColor.whiteColor()
+        navigationController.view.addSubview(maskBgView)
+        navigationController.view.bringSubviewToFront(maskBgView)
+        
+        // logo mask animation
+        // ▼ 5. rootViewController.viewのマスクを少し縮小してから、画面サイズよりも大きくなるよう拡大するアニメーション
+        let transformAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        transformAnimation.delegate = self
+        transformAnimation.duration = 1
+        transformAnimation.beginTime = CACurrentMediaTime() + 1 //add delay of 1 second
+        let initalBounds = NSValue(CGRect: navigationController.view.layer.mask!.bounds)
+        let secondBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 50, height: 50))
+        let finalBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 2000, height: 2000))
+        transformAnimation.values = [initalBounds, secondBounds, finalBounds]
+        transformAnimation.keyTimes = [0, 0.5, 1]
+        transformAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+        transformAnimation.removedOnCompletion = false
+        transformAnimation.fillMode = kCAFillModeForwards
+        navigationController.view.layer.mask!.addAnimation(transformAnimation, forKey: "maskAnimation")
+        
+        // logo mask background view animation
+        // ▼ 6. rootViewController.viewの最前面に配置した白いviewを透化するアニメーション (完了後に親viewから削除)
+        UIView.animateWithDuration(0.1,
+            delay: 1.35,
+            options: UIViewAnimationOptions.CurveEaseIn,
+            animations: {
+                maskBgView.alpha = 0.0
+            },
+            completion: { finished in
+                maskBgView.removeFromSuperview()
+        })
+        
+        // root view animation
+        // ▼ 7. rootViewController.viewを少し拡大して元に戻すアニメーション
+        UIView.animateWithDuration(0.25,
+            delay: 1.3,
+            options: UIViewAnimationOptions.TransitionNone,
+            animations: {
+                self.window!.rootViewController!.view.transform = CGAffineTransformMakeScale(1.05, 1.05)
+            },
+            completion: { finished in
+                UIView.animateWithDuration(0.3,
+                    delay: 0.0,
+                    options: UIViewAnimationOptions.CurveEaseInOut,
+                    animations: {
+                        self.window!.rootViewController!.view.transform = CGAffineTransformIdentity
+                    },
+                    completion: nil
+                )
+        })
         return true
+    }
+    
+    // ▼ 8. 「5.」のアニメーション完了時のdelegateメソッドを実装し、マスクを削除する
+    override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
+        // remove mask when animation completes
+        self.window!.rootViewController!.view.layer.mask = nil
     }
 
     func applicationWillResignActive(application: UIApplication) {
