2ff77795f349e9a2664693d7bdba8d04bdd439da
Koichi <cx.w0077@gmail.com>
2016-03-07 15:34:58 +0800

    デザイン調整

--- secondMovies/BGMViewController.swift
+++ secondMovies/BGMViewController.swift
@@ -96,6 +96,7 @@ class BGMViewController: UIViewController,UITableViewDataSource, UITableViewDele
         var addMusicBtn = cell.viewWithTag(3) as! UIButton
         addMusicBtn.setTitle("決定", forState: .Normal)
         addMusicBtn.tag = 100 + indexPath.row + 1
+        addMusicBtn.backgroundColor = UIColor(red: 245/255, green: 108/255, blue: 102/255, alpha: 1)
 
         return cell
     }
